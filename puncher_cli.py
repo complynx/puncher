834ed072f97daf0e0285c79b84b2b1141c339b43
complynx <complynx@yandex.ru>
2020-04-14 15:51:36 +0300

    initial commit

---
+++ puncher_cli.py
@@ -0,0 +1,94 @@
+import pyautogui
+from pyautogui import Point
+import time
+import json
+import socket
+import sys
+
+
+voicemeeterWinTitle = 'VoiceMeeter'
+vbanTitle = 'VB-Audio Network Configuration'
+puncher_id = "vban_punch_1"
+puncher_addr = ("127.0.0.1", 9999)
+
+minGuiDelay = 0.1
+
+vbanBtnPoint = Point(x=799, y=25)
+vbanWinHeader = Point(x=308, y=9)
+
+vbanInEnable1 = Point(x=38, y=166)
+vbanInAddr1 = Point(x=266, y=165)
+vbanInPort1 = Point(x=382, y=169)
+
+vbanOutEnable1 = Point(x=40, y=539)
+vbanOutAddr1 = Point(x=340, y=541)
+vbanOutPort1 = Point(x=476, y=540)
+
+vbanEnable = Point(x=30, y=53)
+
+vbanWin = None
+
+def addp(p1, p2):
+    return pyautogui.Point(x=p1.x + p2.x, y=p1.y + p2.y)
+def subp(p1, p2):
+    return pyautogui.Point(x=p1.x - p2.x, y=p1.y - p2.y)
+
+try:
+	voicemeeterWin = pyautogui.getAllWindows()[pyautogui.getAllTitles().index(voicemeeterWinTitle)]
+	voicemeeterWin.activate()
+except ValueError:
+	pyautogui.alert("Open VoiceMeeter Banana window before running this program")
+	exit(1)
+
+def vbanSetIn(addr,port):
+    pyautogui.click(addp(vbanWin.topleft, vbanWinHeader))
+    time.sleep(minGuiDelay)
+    pyautogui.click(addp(vbanWin.topleft, vbanInAddr1))
+    time.sleep(minGuiDelay)
+    pyautogui.write(f"{addr}\n")
+    time.sleep(minGuiDelay)
+    pyautogui.click(addp(vbanWin.topleft, vbanInPort1))
+    time.sleep(minGuiDelay)
+    pyautogui.write(f"{port}\n")
+    time.sleep(minGuiDelay)
+    img = pyautogui.screenshot()
+    if img.getpixel(addp(vbanWin.topleft, vbanInEnable1))[1] < 100:
+        pyautogui.click(addp(vbanWin.topleft, vbanInEnable1))
+def vbanSetOut(addr,port):
+    pyautogui.click(addp(vbanWin.topleft, vbanWinHeader))
+    time.sleep(minGuiDelay)
+    pyautogui.click(addp(vbanWin.topleft, vbanOutAddr1))
+    time.sleep(minGuiDelay)
+    pyautogui.write(f"{addr}\n")
+    time.sleep(minGuiDelay)
+    pyautogui.click(addp(vbanWin.topleft, vbanOutPort1))
+    time.sleep(minGuiDelay)
+    pyautogui.write(f"{port}\n")
+    time.sleep(minGuiDelay)
+    img = pyautogui.screenshot()
+    if img.getpixel(addp(vbanWin.topleft, vbanOutEnable1))[1] < 100:
+        pyautogui.click(addp(vbanWin.topleft, vbanOutEnable1))
+def vbanSetEnable():
+    img = pyautogui.screenshot()
+    if img.getpixel(addp(vbanWin.topleft, vbanEnable))[1] < 200:
+        pyautogui.click(addp(vbanWin.topleft, vbanEnable))
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+msg = json.dumps({"punch_id": puncher_id})
+sock.sendto(bytes(msg, "utf-8"), puncher_addr)
+datastr, addr = sock.recvfrom(1024)
+data = json.loads(datastr)
+sock.close()
+
+time.sleep(minGuiDelay)
+pyautogui.click(addp(voicemeeterWin.topleft, vbanBtnPoint))
+vbanWin = pyautogui.getAllWindows()[pyautogui.getAllTitles().index(vbanTitle)]
+time.sleep(minGuiDelay)
+if vbanWin.topleft.x <0 or vbanWin.topleft.y<0:
+    pyautogui.moveTo(addp(vbanWin.topleft, vbanWinHeader))
+    pyautogui.dragTo(310, 40, 0.3, button='left')
+
+vbanSetIn(data["addr"], data["port"])
+vbanSetOut(data["addr"], data["port"])
+vbanSetEnable()
---
+++ puncher_srv.py
@@ -0,0 +1,88 @@
+import logging
+import socket
+import sys
+import json
+import time
+import threading
+
+logger = logging.getLogger()
+addresses = dict()
+
+class Cleaner(threading.Thread):
+    """
+    A thread to dispatch a signal if it is emitted in asynchronous mode.
+    Like in `async_decorator.async`, here the thread gets the info about it's parent thread and logs it up.
+    """
+
+    def __init__(self):
+        """
+        Sets up necessary parameters like thread name, it's parent's name and id, emitter...
+        :param Signal sig: signal reference
+        :param args: arguments of the signal call
+        :param kwargs: KV arguments of the signal call
+        """
+        super(Cleaner, self).__init__(name="thread")
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        """
+        Thread entry point.
+        Logs the thread ancestry.
+        Starts the signal.
+        Logs exceptions if necessary.
+        """
+        while True:
+            time.sleep(60)
+            logger.info("cleaning")
+            now5m = time.time() - (5*60)
+            for k in list(addresses.keys()):
+                if addresses[k]["time"] < now5m:
+                    del addresses[k]
+
+def addr_to_msg(uid, addr):
+    return bytes(json.dumps({
+        "punch_id": str(uid),
+        "addr": addr[0],
+        "port": addr[1]
+    }), "utf-8")
+
+def main(host='0.0.0.0', port=9999):
+    cleaner = Cleaner()
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.bind((host, port))
+    logger.info("listening on %s:%d", host, port)
+    while True:
+        datastr, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        logger.info("connection from: %s", addr)
+        try:
+            data = json.loads(datastr)
+            if "punch_id" in data and isinstance(data["punch_id"], str):
+                pid = data["punch_id"]
+                logger.info("using punch id %s", pid)
+                if pid in addresses:
+                    addr2 = addresses[pid]["addr"]
+
+                    logger.info("sending addr %s to %s", addr2, addr)
+                    msg = addr_to_msg(pid, addr2)
+                    sock.sendto(msg, addr)
+
+                    logger.info("sending addr %s to %s", addr, addr2)
+                    msg = addr_to_msg(pid, addr)
+                    sock.sendto(msg, addr2)
+
+                    del addresses[pid]
+                else:
+                    logger.info("saved addr %s", addr)
+                    addresses[pid] = {
+                        "addr": addr,
+                        "time": time.time()
+                    }
+        except Exception as e:
+            logger.exception("failed to process punch")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+    main()
